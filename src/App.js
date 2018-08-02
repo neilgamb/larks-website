7119319f7fa4fc9298cd446af5e264fb0d4928f1
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2018-08-01 20:53:38 -0400

    Adding in a simple navigation menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core';
+import Navigation from './Navigation';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 
+const theme = createMuiTheme();
+
 export default class App extends Component {
-  state = { menuOpen: false };
+  state = { menuOpen: true };
 
   menuToggle = () => {
     this.setState({ menuOpen: !this.state.menuOpen });
   };
 
   render() {
+    const { menuOpen } = this.state;
     return (
-      <MuiThemeProvider>
+      <MuiThemeProvider theme={theme}>
         <Router>
           <Fragment>
+            <Navigation open={menuOpen} menuToggle={this.menuToggle} />
             <Route path="/home" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Drawer, MenuItem } from '@material-ui/core';
 import { NavLink } from 'react-router-dom';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
 
-const Nav = props => (
-  <Drawer
-    zDepth={1}
-    containerStyle={{
-      zIndex: 1000,
-      marginTop: 60,
-    }}
-    width={200}
-    open={props.menuOpen}
-  >
+const Navigation = props => (
+  <Drawer open={props.open}>
     <NavLink onClick={props.menuToggle} activeclass="active" to="/home">
       <MenuItem style={{ marginTop: 4 }}>Home</MenuItem>
     </NavLink>
@@ -26,13 +17,13 @@ const Nav = props => (
   </Drawer>
 );
 
-Nav.propTypes = {
-  menuOpen: PropTypes.bool,
+Navigation.propTypes = {
+  open: PropTypes.bool,
   menuToggle: PropTypes.func.isRequired,
 };
 
-Nav.defaultProps = {
-  menuOpen: false,
+Navigation.defaultProps = {
+  open: false,
 };
 
-export default Nav;
+export default Navigation;
