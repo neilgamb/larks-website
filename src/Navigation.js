813380300a48f10b12809db564b17250c533a286
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2018-08-04 00:12:36 -0400

    Adding some animation to navigation drawer

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Drawer, MenuItem } from '@material-ui/core';
+import { Drawer, MenuItem, IconButton, withStyles } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import { NavLink } from 'react-router-dom';
 
+const styles = {
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    transition: '250ms',
+  },
+  menuItem: {
+    padding: '20px 100px 20px 25px',
+  },
+  menuItemOne: {
+    padding: '20px 100px 20px 25px',
+    marginTop: 60,
+  },
+  navLink: {
+    textDecoration: 'none',
+  },
+};
+
 const Navigation = props => (
-  <Drawer open={props.open}>
-    <NavLink onClick={props.menuToggle} activeclass="active" to="/home">
-      <MenuItem style={{ marginTop: 4 }}>Home</MenuItem>
+  <Drawer transitionDuration={300} open={props.open}>
+    <IconButton
+      style={{ transform: props.open ? 'none' : 'rotate(45deg)' }}
+      className={props.classes.closeButton}
+      onClick={props.menuToggle}
+    >
+      <Close />
+    </IconButton>
+    <NavLink
+      className={props.classes.navLink}
+      onClick={props.menuToggle}
+      activeclass="active"
+      to="/home"
+    >
+      <MenuItem className={props.classes.menuItemOne}>Home</MenuItem>
     </NavLink>
-    <NavLink onClick={props.menuToggle} activeclass="active" to="/about">
-      <MenuItem>About</MenuItem>
+    <NavLink
+      className={props.classes.navLink}
+      onClick={props.menuToggle}
+      activeclass="active"
+      to="/about"
+    >
+      <MenuItem className={props.classes.menuItem}>About</MenuItem>
     </NavLink>
-    <NavLink onClick={props.menuToggle} activeclass="active" to="/contact">
-      <MenuItem>Contact</MenuItem>
+    <NavLink
+      className={props.classes.navLink}
+      onClick={props.menuToggle}
+      activeclass="active"
+      to="/contact"
+    >
+      <MenuItem className={props.classes.menuItem}>Contact</MenuItem>
     </NavLink>
   </Drawer>
 );
@@ -20,10 +62,11 @@ const Navigation = props => (
 Navigation.propTypes = {
   open: PropTypes.bool,
   menuToggle: PropTypes.func.isRequired,
+  classes: PropTypes.object,
 };
 
 Navigation.defaultProps = {
   open: false,
 };
 
-export default Navigation;
+export default withStyles(styles)(Navigation);
